0eb34decef0edc689250366415e5f29efbcd2309
kokooldude <khalileoliver@gmail.com>
2020-09-09 15:35:11 +0000

    I completed the tank!

--- index.java
+++ index.java
@@ -14,12 +14,13 @@ void draw() {
   //this rectangle is the start of your army tank
   //all of your programming should be after this 👍 
   rect(200, 200, 100, 50);
-  
-  
- 
-
-  
- 
-	
+  rect(175, 161 , 50, 24);
+  ellipse(150, 225, 20, 20)
+  ellipse(170, 225, 20, 20)
+ ellipse(190, 225, 20, 20)
+ellipse(211, 225, 20, 20)
+  ellipse(231, 225, 20, 20)
+ ellipse(250, 225, 20, 20)
+	rect(262, 157, 125, 12)
 
 }
